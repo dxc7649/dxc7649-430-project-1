40eeaa16b9fc4d1dc52371e738e5595e1026f005
David Chen <dxc7649@g.rit.edu>
2021-03-09 01:37:50 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -12,96 +12,96 @@ const jsonHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const handlePost = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/addTask') {
-        const body = [];
+  if (parsedUrl.pathname === '/addTask') {
+    const body = [];
 
-        request.on('error', (err) => {
-            console.dir(err);
-            response.statusCode = 400;
-            response.end();
-        });
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
 
-        request.on('end', () => {
-            const tasksString = Buffer.concat(body).toString();
-            const tasksParams = query.parse(tasksString);
+    request.on('end', () => {
+      const tasksString = Buffer.concat(body).toString();
+      const tasksParams = query.parse(tasksString);
 
-            jsonHandler.addTask(request, response, tasksParams);
-        });
-    } else if (parsedUrl.pathname === '/addUsers') {
-        const body = [];
+      jsonHandler.addTask(request, response, tasksParams);
+    });
+  } else if (parsedUrl.pathname === '/addUsers') {
+    const body = [];
 
-        request.on('error', (err) => {
-            console.dir(err);
-            response.statusCode = 400;
-            response.end();
-        });
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
 
-        request.on('end', () => {
-            const usersString = Buffer.concat(body).toString();
-            const usersParams = query.parse(usersString);
+    request.on('end', () => {
+      const usersString = Buffer.concat(body).toString();
+      const usersParams = query.parse(usersString);
 
-            jsonHandler.addUsers(request, response, usersParams);
-        });
-    }
+      jsonHandler.addUsers(request, response, usersParams);
+    });
+  }
 };
 
 const handleGet = (request, response, parsedUrl, acceptedTypes, params) => {
-    if (parsedUrl.pathname === '/style.css') {
-        htmlHandler.getCSS(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/getTasks') {
-        jsonHandler.getTasks(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/getUsers') {
-        jsonHandler.getUsers(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/' || parsedUrl.pathname === '/welcome.html') {
-        htmlHandler.getWelcomeResponse(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/users.html') {
-        htmlHandler.getUsersResponse(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/task.html') {
-        htmlHandler.getMainResponse(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/admin.html') {
-        htmlHandler.getAdminResponse(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/list.png') {
-        jsonHandler.getImage(request, response, acceptedTypes, request.method, params);
-    } else {
-        htmlHandler.getErrorResponse(request, response);
-    }
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/getTasks') {
+    jsonHandler.getTasks(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/' || parsedUrl.pathname === '/welcome.html') {
+    htmlHandler.getWelcomeResponse(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/users.html') {
+    htmlHandler.getUsersResponse(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/task.html') {
+    htmlHandler.getMainResponse(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/admin.html') {
+    htmlHandler.getAdminResponse(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/list.png') {
+    jsonHandler.getImage(request, response, acceptedTypes, request.method, params);
+  } else {
+    htmlHandler.getErrorResponse(request, response);
+  }
 };
 
 const handleHead = (request, response, parsedUrl, acceptedTypes, params) => {
-    if (parsedUrl.pathname === '/getTasks') {
-        jsonHandler.getTasks(request, response, acceptedTypes, request.method, params);
-    } else if (parsedUrl.pathname === '/getUsers') {
-        jsonHandler.getUsers(request, response, acceptedTypes, request.method, params);
-    }
+  if (parsedUrl.pathname === '/getTasks') {
+    jsonHandler.getTasks(request, response, acceptedTypes, request.method, params);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response, acceptedTypes, request.method, params);
+  }
 };
 
 const onRequest = (request, response) => {
-    let acceptedTypes = request.headers.accept.split(',');
-    acceptedTypes = acceptedTypes || [];
+  let acceptedTypes = request.headers.accept.split(',');
+  acceptedTypes = acceptedTypes || [];
 
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    const params = query.parse(parsedUrl.query);
-    const {
-        limit,
-    } = params;
-    console.log('limit=', limit);
+  const params = query.parse(parsedUrl.query);
+  const {
+    limit,
+  } = params;
+  console.log('limit=', limit);
 
-    if (request.method === 'POST') {
-        handlePost(request, response, parsedUrl);
-    } else if (request.method === 'GET') {
-        handleGet(request, response, parsedUrl, acceptedTypes, params);
-    } else if (request.method === 'HEAD') {
-        handleHead(request, response, parsedUrl, acceptedTypes, params);
-    }
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'GET') {
+    handleGet(request, response, parsedUrl, acceptedTypes, params);
+  } else if (request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl, acceptedTypes, params);
+  }
 };
 
 http.createServer(onRequest).listen(port); // method chaining!
--- src/responses.js
+++ src/responses.js
@@ -8,120 +8,120 @@ const multiUsers = [];
 const getBinarySize = (string) => Buffer.byteLength(string, 'utf8');
 
 const loadFile = (request, response, filePath, fileType) => {
-    const file = path.resolve(__dirname, filePath);
+  const file = path.resolve(__dirname, filePath);
 
-    fs.stat(file, (err, stats) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                response.writeHead(404);
-            }
-            return response.end(err);
-        }
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
 
-        let {
-            range,
-        } = request.headers;
+    let {
+      range,
+    } = request.headers;
 
-        if (!range) {
-            range = 'bytes=0-';
-        }
+    if (!range) {
+      range = 'bytes=0-';
+    }
 
-        // remove the part "bytes=" then split the number with '-'
-        const positions = range.replace(/bytes=/, '').split('-');
+    // remove the part "bytes=" then split the number with '-'
+    const positions = range.replace(/bytes=/, '').split('-');
 
-        // Parse the first part ['0000', '0001'](the start) into integer
-        let start = parseInt(positions[0], 10);
+    // Parse the first part ['0000', '0001'](the start) into integer
+    let start = parseInt(positions[0], 10);
 
-        const total = stats.size;
-        const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
-        if (start > end) {
-            start = end - 1;
-        }
+    if (start > end) {
+      start = end - 1;
+    }
 
-        const chunksize = (end - start) + 1;
+    const chunksize = (end - start) + 1;
 
-        response.writeHead(206, {
-            'Content-Range': `bytes ${start}-${end}/${total}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': fileType,
-        });
-
-        const stream = fs.createReadStream(file, {
-            start,
-            end,
-        });
-
-        stream.on('open', () => {
-            stream.pipe(response);
-        });
-
-        stream.on('error', (streamErr) => {
-            response.end(streamErr);
-        });
-
-        return stream;
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': fileType,
     });
+
+    const stream = fs.createReadStream(file, {
+      start,
+      end,
+    });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
 };
 
 const getImage = (request, response) => {
-    loadFile(request, response, '../client/list.png', 'image/png');
+  loadFile(request, response, '../client/list.png', 'image/png');
 };
 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, {
-        'Content-Type': 'application/json',
-    });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+  });
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    response.writeHead(status, {
-        'Content-Type': 'application/json',
-    });
-    response.end();
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+  });
+  response.end();
 };
 
 const respondMeta = (request, response, type, byteLength) => {
-    const headers = {
-        'Content-Type': type,
-        'Content-Length': byteLength,
-    };
+  const headers = {
+    'Content-Type': type,
+    'Content-Length': byteLength,
+  };
 
-    response.writeHead(200, headers);
-    response.end();
+  response.writeHead(200, headers);
+  response.end();
 };
 
-// Response for "getTasks" endpoint 
-const getTasks = (request, response, acceptedTypes, httpmethod, params) => {
-    if (acceptedTypes.includes('text/xml') === true && httpmethod === 'GET') {
-        const taskXML = multiTasks;
+// Response for "getTasks" endpoint
+const getTasks = (request, response, acceptedTypes, httpmethod) => {
+  if (acceptedTypes.includes('text/xml') === true && httpmethod === 'GET') {
+    const taskXML = multiTasks;
 
-        response.writeHead(200, {
-            'Content-Type': 'text/xml',
-        });
+    response.writeHead(200, {
+      'Content-Type': 'text/xml',
+    });
 
-        response.write('<Tasks>');
-        for (let i = 0; i < taskXML.length; i += 1) {
-            response.write(`
+    response.write('<Tasks>');
+    for (let i = 0; i < taskXML.length; i += 1) {
+      response.write(`
                 <Task>
                     <user>${taskXML[i].user}</user>
                     <task>${taskXML[i].task}</task>
                     <time>${taskXML[i].time}</time>
                 </Task>
             `);
-        }
-        response.write('</Tasks>');
+    }
+    response.write('</Tasks>');
 
-        response.end();
-    } else if (acceptedTypes.includes('text/xml') === true && httpmethod === 'HEAD') {
-        const taskXML = multiTasks;
-        let byteSize = 0;
+    response.end();
+  } else if (acceptedTypes.includes('text/xml') === true && httpmethod === 'HEAD') {
+    const taskXML = multiTasks;
+    let byteSize = 0;
 
-        for (let i = 0; i < taskXML.length; i += 1) {
-            const byteXML = `
+    for (let i = 0; i < taskXML.length; i += 1) {
+      const byteXML = `
                 <Task>
                     <user>${taskXML[i].user}</user>
                     <task>${taskXML[i].task}</task>
@@ -129,167 +129,167 @@ const getTasks = (request, response, acceptedTypes, httpmethod, params) => {
                 </Task>
             `;
 
-            byteSize += getBinarySize(byteXML);
-        }
-
-        return respondMeta(request, response, 'text/xml', byteSize);
-    } else if (httpmethod === 'GET') {
-        const taskString = JSON.stringify(multiTasks);
-        response.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-        response.write(taskString);
-        response.end();
-
-        const taskJSON = JSON.stringify(multiTasks);
-
-        return respondMeta(request, response, 'application/json', getBinarySize(taskJSON));
+      byteSize += getBinarySize(byteXML);
     }
 
+    return respondMeta(request, response, 'text/xml', byteSize);
+  } else if (httpmethod === 'GET') {
     const taskString = JSON.stringify(multiTasks);
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    response.write(taskString);
+    response.end();
 
-    return respondMeta(request, response, 'application/json', getBinarySize(taskString));
+    const taskJSON = JSON.stringify(multiTasks);
+
+    return respondMeta(request, response, 'application/json', getBinarySize(taskJSON));
+  }
+
+  const taskString = JSON.stringify(multiTasks);
+
+  return respondMeta(request, response, 'application/json', getBinarySize(taskString));
 };
 
 // Parameter function for "getUsers"
 const getUserJSON = (limit = multiUsers.length) => {
-    let limit2 = Number(limit);
-    const usersLength = multiUsers.length;
+  let limit2 = Number(limit);
+  const usersLength = multiUsers.length;
 
-    limit2 = !limit2 ? 1 : limit;
-    limit2 = limit2 < 1 ? 1 : limit2;
-    limit2 = limit2 > usersLength ? usersLength - 1 : limit2;
-    
-    const userKeys = [...multiUsers.keys()];
-    
-    const sliceKeys = userKeys.slice(0, limit2);
-    
-    const limitUser = [];
-    sliceKeys.forEach((keys) => {
-        limitUser.push(multiUsers[keys]);
-    });
-    
-    return limitUser;
+  limit2 = !limit2 ? 1 : limit;
+  limit2 = limit2 < 1 ? 1 : limit2;
+  limit2 = limit2 > usersLength ? usersLength - 1 : limit2;
+
+  const userKeys = [...multiUsers.keys()];
+
+  const sliceKeys = userKeys.slice(0, limit2);
+
+  const limitUser = [];
+  sliceKeys.forEach((keys) => {
+    limitUser.push(multiUsers[keys]);
+  });
+
+  return limitUser;
 };
 
 // Respones for "getUsers" endpoint
 const getUsers = (request, response, acceptedTyeps, httpmethod, params) => {
-    if (acceptedTyeps.includes('text/xml') === true && httpmethod === 'GET') {
-        const userXML = getUserJSON(params.limit);
+  if (acceptedTyeps.includes('text/xml') === true && httpmethod === 'GET') {
+    const userXML = getUserJSON(params.limit);
 
-        response.writeHead(200, {
-            'Content-Type': 'text/xml',
-        });
+    response.writeHead(200, {
+      'Content-Type': 'text/xml',
+    });
 
-        response.write('<Usernames>');
-        for (let i = 0; i < userXML.length; i += 1) {
-            response.write(`
+    response.write('<Usernames>');
+    for (let i = 0; i < userXML.length; i += 1) {
+      response.write(`
                 <Username>
                     <user>${userXML[i]}</user>
                 </Username>
             `);
-        }
-        response.write('</Usernames>');
+    }
+    response.write('</Usernames>');
 
-        response.end();
-    } else if (acceptedTyeps.includes('text/xml') === true && httpmethod === 'HEAD') {
-        const userXML = getUserJSON(params.limit);
-        let byteSize = 0;
+    response.end();
+  } else if (acceptedTyeps.includes('text/xml') === true && httpmethod === 'HEAD') {
+    const userXML = getUserJSON(params.limit);
+    let byteSize = 0;
 
-        for (let i = 0; i < userXML.length; i += 1) {
-            const byteXML = `
+    for (let i = 0; i < userXML.length; i += 1) {
+      const byteXML = `
                 <Username>
                     <user>${userXML[i]}</user>
                 </Username>
             `;
 
-            byteSize += getBinarySize(byteXML);
-        }
-
-        return respondMeta(request, response, 'text/xml', byteSize);
-    } else if (httpmethod === 'GET') {
-        const userString = JSON.stringify(getUserJSON(params.limit));
-        response.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-        response.write(userString);
-        response.end();
-
-        const userJSON = JSON.stringify(getUserJSON(params.limit));
-
-        return respondMeta(request, response, 'application/json', getBinarySize(userJSON));
+      byteSize += getBinarySize(byteXML);
     }
 
+    return respondMeta(request, response, 'text/xml', byteSize);
+  } else if (httpmethod === 'GET') {
     const userString = JSON.stringify(getUserJSON(params.limit));
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    response.write(userString);
+    response.end();
 
-    return respondMeta(request, response, 'application/json', getBinarySize(userString));
+    const userJSON = JSON.stringify(getUserJSON(params.limit));
+
+    return respondMeta(request, response, 'application/json', getBinarySize(userJSON));
+  }
+
+  const userString = JSON.stringify(getUserJSON(params.limit));
+
+  return respondMeta(request, response, 'application/json', getBinarySize(userString));
 };
 
 // Function for adding tasks to To-do list
 const addTask = (request, response, body) => {
-    const responseJSON = {
-        message: 'Required all three fields: username, task, and time! Plase try again!',
-    };
+  const responseJSON = {
+    message: 'Required all three fields: username, task, and time! Plase try again!',
+  };
 
-    if (!body.tasks || !body.times || !body.users) {
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON);
+  if (!body.tasks || !body.times || !body.users) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (multiTasks.find((element) => element.task === body.tasks)
+        && multiTasks.find((element) => element.user === body.users)) {
+    responseCode = 204;
+
+    for (let i = 0; i < multiTasks.length; i += 1) {
+      if (multiTasks[i].task === body.tasks && multiTasks[i].user === body.users) {
+        multiTasks[i].time = body.times;
+        break;
+      }
     }
+  } else {
+    const newUser = {};
+    newUser.user = body.users;
+    newUser.task = body.tasks;
+    newUser.time = body.times;
 
-    let responseCode = 201;
+    multiTasks.push(newUser);
+  }
 
-    if (multiTasks.find((element) => element.task === body.tasks) &&
-        multiTasks.find((element) => element.user === body.users)) {
-        responseCode = 204;
+  if (responseCode === 201) {
+    responseJSON.message = 'Task is added!';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-        for (let i = 0; i < multiTasks.length; i += 1) {
-            if (multiTasks[i].task === body.tasks && multiTasks[i].user === body.users) {
-                multiTasks[i].time = body.times;
-                break;
-            }
-        }
-    } else {
-        const newUser = {};
-        newUser.user = body.users;
-        newUser.task = body.tasks;
-        newUser.time = body.times;
-
-        multiTasks.push(newUser);
-    }
-
-    if (responseCode === 201) {
-        responseJSON.message = 'Task is added!';
-        return respondJSON(request, response, responseCode, responseJSON);
-    }
-
-    return respondJSONMeta(request, response, responseCode);
+  return respondJSONMeta(request, response, responseCode);
 };
 
 // Function for adding new users
 const addUsers = (request, response, body) => {
-    const responseJSON = {
-        message: 'Required a username! Plase try again',
-    };
+  const responseJSON = {
+    message: 'Required a username! Plase try again',
+  };
 
-    if (!body.users) {
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON);
-    }
+  if (!body.users) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
 
-    let responseCode = 201;
+  let responseCode = 201;
 
-    if (multiUsers.find((element) => element === body.users)) {
-        responseCode = 204;
-    } else {
-        multiUsers.push(body.users);
-    }
+  if (multiUsers.find((element) => element === body.users)) {
+    responseCode = 204;
+  } else {
+    multiUsers.push(body.users);
+  }
 
-    if (responseCode === 201) {
-        responseJSON.message = 'New User is added!';
-        return respondJSON(request, response, responseCode, responseJSON);
-    }
+  if (responseCode === 201) {
+    responseJSON.message = 'New User is added!';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-    return respondJSONMeta(request, response, responseCode);
+  return respondJSONMeta(request, response, responseCode);
 };
 
 module.exports.getTasks = getTasks;
